02982d0396990ee058cfa4a276330230df960518
Mikolaj Sowinski <mikolaj.sowinski@gmail.com>
2021-07-26 13:53:12 +0200

    Fixed typo in Xilinx Vivado base platform file

--- migen/build/xilinx/vivado.py
+++ migen/build/xilinx/vivado.py
@@ -81,7 +81,7 @@ class XilinxVivadoToolchain:
     }
 
     def __init__(self):
-        self.postsynthesis_commands = []
+        self.post_synthesis_commands = []
         self.bitstream_commands = []
         self.additional_commands = []
         self.pre_synthesis_commands = []
@@ -119,8 +119,8 @@ class XilinxVivadoToolchain:
         tcl.append("report_timing_summary -file {}_timing_synth.rpt".format(build_name))
         tcl.append("report_utilization -hierarchical -file {}_utilization_hierarchical_synth.rpt".format(build_name))
         tcl.append("report_utilization -file {}_utilization_synth.rpt".format(build_name))
-        for postsynthesis_command in self.postsynthesis_commands:
-            tcl.append(postsynthesis_command)
+        for post_synthesis_command in self.post_synthesis_commands:
+            tcl.append(post_synthesis_command)
         tcl.append("opt_design")
         tcl.append("place_design")
         if self.with_phys_opt:
